d0340f5fd4e92fddba34c73b2d462bf8c26570f5
Martijn de Bont <mkdebont@gmail.com>
2016-02-12 10:00:19 +0100

    Settings

--- Lark3dSlicer/Properties/Settings.Designer.cs
+++ Lark3dSlicer/Properties/Settings.Designer.cs
@@ -8,22 +8,66 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Lark3dSlicer.Properties
-{
-
-
+namespace Lark3dSlicer.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.6")]
+        public double NozzleDiameter1 {
+            get {
+                return ((double)(this["NozzleDiameter1"]));
+            }
+            set {
+                this["NozzleDiameter1"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.75")]
+        public double FilamentDiameter {
+            get {
+                return ((double)(this["FilamentDiameter"]));
+            }
+            set {
+                this["FilamentDiameter"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public int NumberOfExtruders {
+            get {
+                return ((int)(this["NumberOfExtruders"]));
+            }
+            set {
+                this["NumberOfExtruders"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.2")]
+        public double NozzleDiameter2 {
+            get {
+                return ((double)(this["NozzleDiameter2"]));
+            }
+            set {
+                this["NozzleDiameter2"] = value;
             }
         }
     }
